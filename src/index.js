d4eaa57bb0645406c94929413af46b10d6443930
Vincent <vkammerer@gmail.com>
2017-05-29 15:43:51 -0400

    Removes debug option and makes ping and pong hooks optional

--- src/index.js
+++ src/index.js
@@ -2,52 +2,45 @@ import { mainMessager, workerMessager } from "@vkammerer/postmessage-raf";
 // import { mainMessager, workerMessager } from "../../postmessage-raf";
 import { logWithPerf } from "./utils";
 
-export const createMainMiddleware = ({ debug, worker }) => store => {
+export const createMainMiddleware = (worker, options) => store => {
   const messager = mainMessager({
     worker,
     onAction: store.dispatch,
-    beforePing: pingCount =>
-      store.dispatch({ type: "PING_BEFORE", payload: pingCount }),
-    afterPing: pingCount =>
-      store.dispatch({ type: "PING_AFTER", payload: pingCount })
+    beforePing: !options || !options.dispatchBeforePing
+      ? undefined
+      : count => store.dispatch({ type: "PING_BEFORE", payload: count }),
+    afterPing: !options || !options.dispatchAfterPing
+      ? undefined
+      : count => store.dispatch({ type: "PING_AFTER", payload: count })
   });
   return next =>
     function handleActionInMiddleware(action) {
-      if (action.meta && action.meta.toWorker) {
-        if (debug) logWithPerf("TO WORKER  ", action);
-        return messager.post(action);
-      }
-      if (action.meta && action.meta.toMain) {
-        if (debug) logWithPerf("FROM WORKER", action);
-        return next(action);
-      }
-      if (debug) logWithPerf("SELF MAIN  ", action);
+      if (action.meta && action.meta.toMain) return next(action);
+      if (action.meta && action.meta.toWorker) messager.post(action);
+      if (action.meta && (action.meta.delay || action.meta.ignoreSelf)) return;
       return next(action);
     };
 };
 
-export const createWorkerMiddleware = ({ debug }) => store => {
+export const createWorkerMiddleware = options => store => {
   const messager = workerMessager({
     onAction: store.dispatch,
-    beforePong: pongCount =>
-      store.dispatch({ type: "PONG_BEFORE", payload: pongCount }),
-    afterPong: pongCount =>
-      store.dispatch({ type: "PONG_AFTER", payload: pongCount })
+    beforePong: !options || !options.dispatchBeforePong
+      ? undefined
+      : count => store.dispatch({ type: "PONG_BEFORE", payload: count }),
+    afterPong: !options || !options.dispatchAfterPong
+      ? undefined
+      : count => store.dispatch({ type: "PONG_AFTER", payload: count })
   });
   return next =>
     function handleActionInMiddleware(action) {
+      if (action.meta && action.meta.toWorker) return next(action);
       if (action.meta && action.meta.toMain) {
-        if (debug) logWithPerf("TO MAIN    ", action);
         messager.post(action, { delay: action.meta.delay });
         if (action.type === "PING_START") messager.startPing();
         if (action.type === "PING_STOP") messager.stopPing();
-        return;
       }
-      if (action.meta && action.meta.toWorker) {
-        if (debug) logWithPerf("FROM MAIN  ", action);
-        return next(action);
-      }
-      if (debug) logWithPerf("SELF WORKER", action);
+      if (action.meta && (action.meta.delay || action.meta.ignoreSelf)) return;
       return next(action);
     };
 };
